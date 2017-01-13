7ba363611030f16b8419a97588d58d4ee4ce781c
Ashwini <ashie.oh@gmail.com>
2017-01-13 17:23:40 +0000

    added first test

--- roman_numerals/spec/roman_numerals_spec.rb
+++ roman_numerals/spec/roman_numerals_spec.rb
@@ -1,1 +1,8 @@
-require 'roman_numerals' # that is the roman_numerals.rb file that we are testing
+# has to give relative path.rspec is not recogonising without the relative path
+require '../lib/roman_numerals' # that is the roman_numerals.rb file that we are testing
+
+describe "Roman_numerals" do
+  it "outputs I when given 1" do
+    expect(roman_numerals(1)).to eq "I"
+  end
+end

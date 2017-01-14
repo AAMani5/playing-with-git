dff1b6cb725b0569ba4845c3c171582e1062f33e
Ashwini <ashie.oh@gmail.com>
2017-01-14 10:31:24 +0000

    added 2 more simple test & made it pass

--- roman_numerals/lib/roman_numerals.rb
+++ roman_numerals/lib/roman_numerals.rb
@@ -1,3 +1,4 @@
 def roman_numerals(decimal)
-  "I"
+  decimal_to_roman = {1 => "I", 4 => "IV", 5 => "V"}
+  decimal_to_roman[decimal]
 end
--- roman_numerals/spec/roman_numerals_spec.rb
+++ roman_numerals/spec/roman_numerals_spec.rb
@@ -5,4 +5,10 @@ describe "Roman_numerals" do
   it "outputs I when given 1" do
     expect(roman_numerals(1)).to eq "I"
   end
+  it "outputs IV when given 4" do
+    expect(roman_numerals(4)).to eq "IV"
+  end
+  it "outputs V when given 5" do
+    expect(roman_numerals(5)).to eq "V"
+  end
 end

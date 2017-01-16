d9c402713bef9547bf11a771e4cfbe9d96236191
Ashwini <ashie.oh@gmail.com>
2017-01-16 12:29:51 +0000

    completed the exercise & rspec runs with no errors

--- roman_numerals/lib/roman_numerals.rb
+++ roman_numerals/lib/roman_numerals.rb
@@ -1,4 +1,8 @@
 def roman_numerals(decimal)
   decimal_to_roman = {1000 => "M", 900 => "CM", 500 => "D", 400 => "CD", 100=> "C", 90 => "XC", 50 => "L", 40 => "XL", 10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I"}
-  decimal_to_roman[decimal]
+  remaining = decimal
+  decimal_to_roman.inject("") do |result,conversion|
+    whole, remaining = remaining.divmod(conversion[0])
+    result << conversion[1] * whole
+  end
 end
